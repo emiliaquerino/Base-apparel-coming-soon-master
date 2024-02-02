a16ab217d7dcb29d2a93e5cf8fda9a2f948750dd
Emília Querino <emiliaquerino@gmail.com>
2024-02-02 14:57:23 -0300

    Add um alert de sucesso para o envio

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const submit = document.querySelector(".submit")
-const error = document.querySelector(".error")
-const form = document.querySelector("form")
-const input = document.querySelector("input")
-const label = document.querySelector("label")
+const submit = document.querySelector(".submit");
+const error = document.querySelector(".error");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const label = document.querySelector("label");
 
-form.addEventListener("submit", validate)
-submit.addEventListener("click", validate)
+form.addEventListener("submit", validate);
+submit.addEventListener("click", validate);
 
 function validate(e) {
-  e.preventDefault()
+  e.preventDefault();
 
-  const inputValue = input.value.trim()
+  const inputValue = input.value.trim();
 
   if (!isEmail(inputValue)) {
-    error.style.display = "block"
-    label.style.display = "block"
+    error.style.display = "block";
+    label.style.display = "block";
   } else {
-    error.style.display = "none"
-    label.style.display = "none"
+    error.style.display = "none";
+    label.style.display = "none";
+    alert("Enviado com sucesso!");
   }
 }
 
 function isEmail(input) {
-  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)
+  return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input);
 }
